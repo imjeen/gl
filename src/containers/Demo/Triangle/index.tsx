b00c8c0f2f4fd52fbb1ac93f2a716e7b94598417
imjeen <imjeen@sina.com>
2021-02-23 15:04:33 +0800

    :zap: use the common code with utils

--- src/containers/Demo/Triangle/index.tsx
+++ src/containers/Demo/Triangle/index.tsx
@@ -1,79 +1,11 @@
 import React from 'react';
 
 import { useCanvas } from '@/services/hooks/canvas';
+import { initWebGL } from '@/utils/gl';
 
 export default function Triangle() {
     const refCanvas = useCanvas<WebGLRenderingContext>(render);
     return <canvas ref={refCanvas} width="750" height="1000" />;
-}
-
-/**
- * 创建 shader
- *
- * @param {WebGLRenderingContext} gl
- * @param {number} type
- * @param {string} source
- */
-function createShader(gl: WebGLRenderingContext, type: number, source: string) {
-    // 创建 shader 对象
-    let shader = gl.createShader(type);
-    if (!shader) return;
-    // 往 shader 中传入源代码
-    gl.shaderSource(shader, source);
-    // 编译 shader
-    gl.compileShader(shader);
-    // 判断 shader 是否编译成功
-    let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-    if (success) {
-        return shader;
-    }
-    // 如果编译失败，则打印错误信息
-    console.log(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-}
-
-/**
- * 创建 program
- *
- * @param {WebGLRenderingContext} gl
- * @param {WebGLShader} vertexShader
- * @param {WebGLShader} fragmentShader
- */
-function createProgram(gl: WebGLRenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader) {
-    // 创建 program 对象
-    let program = gl.createProgram();
-    if (!program) return;
-    // 往 program 对象中传入 WebGLShader 对象
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    // 链接 program
-    gl.linkProgram(program);
-    // 判断 program 是否链接成功
-    let success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-        return program;
-    }
-    // 如果 program 链接失败，则打印错误信息
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
-}
-
-/**
- * 初始化Shader程序
- *
- * @param {WebGLRenderingContext} gl
- * @param {string} vertexSource
- * @param {string} fragmentSource
- */
-function initWebGL(gl: WebGLRenderingContext, vertexSource: string, fragmentSource: string) {
-    // 根据源代码创建顶点着色器
-    let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexSource);
-    // 根据源代码创建片元着色器
-    let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
-    if (!vertexShader || !fragmentShader) return;
-    // 创建 WebGL Program 程序
-    let program = createProgram(gl, vertexShader, fragmentShader);
-    return program;
 }
 
 // 顶点着色器
